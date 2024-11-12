7805107eaafcc385ab13f96584c0fc1cd63e2169
DavydaVladyslav <vladyslav.davyda.ki.2021@lpnu.ua>
2024-11-12 23:41:47 +0200

    added changes documentation

---
+++ dox/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buildjson_0',['buildJSON',['../mainwindow_8cpp.html#ad2e76bc0007ae507cb5c2904931babcc',1,'buildJSON():&#160;mainwindow.cpp'],['../server__lab3_8ino.html#afe2f8edf43ea01c073673d937d67d0d4',1,'buildJSON():&#160;server_lab3.ino']]]
+];
